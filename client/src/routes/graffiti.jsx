56dbcfafb67f9a64d8b6fed770f3cabc0523ca39
anthonydibi <ajdibiaggio@gmail.com>
2022-07-01 14:49:20 -0500

    eslint

--- client/src/routes/graffiti.jsx
+++ client/src/routes/graffiti.jsx
@@ -1,14 +1,13 @@
 import { Center } from "@chakra-ui/react"
 import GraffitiCanvas from '../components/GraffitiCanvas'
-import { useLayoutEffect } from 'react';
 import GraffitiModal from '../components/GraffitiModal'
 
 export default function Graffiti(){
 
     let showModal = false;
 
-    const hasVisitedGraffiti = localStorage.getItem("hasVisitedGraffiti")
-    if (hasVisitedGraffiti != 1) {
+    let hasVisitedGraffiti = localStorage.getItem("hasVisitedGraffiti")
+    if (hasVisitedGraffiti !== "1") {
         localStorage.setItem("hasVisitedGraffiti", "1");
         showModal = true;
     }
