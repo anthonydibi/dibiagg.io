c952d290dad07c98de698b7a813b507a7792d516
anthonydibi <ajdibiaggio@gmail.com>
2022-07-07 18:45:59 -0500

    Better icon cards, formatting on about page

--- client/src/components/IconCard.js
+++ client/src/components/IconCard.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import {Flex, useColorModeValue, Image, Text} from '@chakra-ui/react';
+import {Flex, useColorModeValue, Image, Text, Heading} from '@chakra-ui/react';
 
 export default function IconCard(props) {
     return (
         <Flex
         bg={useColorModeValue(props.color, "gray.800")}
         boxShadow={"xl"}
-        rounded={"lg"}
+        rounded={"2xl"}
         width={"100%"}
         height={"100%"}
-        p={"6"}
-        align={"center"}
+        p={"10"}
         flexDirection={"column"}
-        justify={"space-evenly"}>
+        justify={"start"}
+        gap={"15px"}>
             <Image 
             boxShadow={"md"}
             src={props.src}
-            boxSize={"100px"}
+            boxSize={"70px"}
             borderRadius={props.rounded === "true" ? "full" : "none"}
-            pos={"relative"}
-            mb={"6"}>
+            pos={"relative"}>
             </Image>
+            <Heading size={"md"}>
+                {props.heading}
+            </Heading>
             <Text fontSize={{ base: "md", sm: "lg", md: "xl" }} width={"80%"}>
                 {props.text}
             </Text>
--- client/src/routes/resume.jsx
+++ client/src/routes/resume.jsx
@@ -4,7 +4,7 @@ import { Center, Box } from '@chakra-ui/react'
 export default function Resume(){
     return (
         <>
-        <Box h="100px"></Box>
+        <Box h="100px" w="50%"></Box>
             <Center>
                 <PdfViewer filename={"Anthony Di Biaggio Resume.pdf"} />
             </Center>
