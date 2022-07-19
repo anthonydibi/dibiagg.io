2b9b15337947eea599c7027a564f103417f44cc3
anthonydibi <ajdibiaggio@gmail.com>
2022-07-19 13:25:27 -0500

    Leaderboard first pass

--- client/src/routes/deathballleaderboard.jsx
+++ client/src/routes/deathballleaderboard.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
-import { Box } from "@chakra-ui/react";
+import { Stack, Box, Th, Tr, Table, Thead, Center, Td, Tbody, TableContainer, Flex, Heading, useColorModeValue, IconButton } from "@chakra-ui/react";
+import { GiCrenelCrown } from 'react-icons/gi'
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
 
 
 export default function DeathballLeaderboard(){
@@ -32,7 +34,7 @@ export default function DeathballLeaderboard(){
 
     useEffect(() => {
         fetchStandings(start, stop);
-    })
+    }, [start, stop])
 
     const next = () => {
         if(start + 15 < max){
@@ -42,17 +44,57 @@ export default function DeathballLeaderboard(){
     }
 
     const back = () => {
-        if(start - 15 <= 0){
+        if(start - 15 >= 0){
             setStart(start - 15);
             setStop(stop - 15);
         }
     }
+
+    const LeaderboardEntry = (player) => {
+
+    }
      
     return (
         <>
-            <Box w="80%">
-                test
-            </Box>
+            <Box w="100%" h="100px"></Box>
+            <Center>
+                <Flex direction={"column"} mb="5" w="300px" h="200px" justify={"center"} align={"center"}>
+                    <GiCrenelCrown size={100}/>
+                    <Heading>{standings[0]?.name.toUpperCase()}</Heading>
+                </Flex>
+            </Center>
+            <Center>
+            <Stack w={{base: "90%", md: "60%"}} border="1px solid" boxShadow={"xl"}>
+                <TableContainer>
+                    <Table variant="striped" colorScheme="gray">
+                        <Thead>
+                            <Tr>
+                                <Th>Rank</Th>
+                                <Th>Player</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                        {standings.slice(1).map((player) => {
+                            return (
+                                <Tr>
+                                    <Td>{player.rank}</Td>
+                                    <Td>{player.name.toUpperCase()}</Td>
+                                </Tr>
+                            );
+                        })}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+                <Stack p={1} mt="5" direction={"row"} w="100%" justify={"right"} align={"center"}>
+                    <IconButton rounded='full' icon={<AiFillCaretLeft/>} onClick={back}>
+
+                    </IconButton>
+                    <IconButton rounded="full" icon={<AiFillCaretRight/>} onClick={next}>
+
+                    </IconButton>
+                </Stack>
+            </Stack>
+            </Center>
         </>
     );
 }
