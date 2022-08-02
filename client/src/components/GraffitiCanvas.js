435de9da92df86d5cc93de8f8b777fc705fe89c8
Anthony Di Biaggio <ajdibiaggio@gmail.com>
2022-08-02 18:13:51 -0500

    rm websocket port

--- client/src/components/GraffitiCanvas.js
+++ client/src/components/GraffitiCanvas.js
@@ -130,7 +130,7 @@ export default function GraffitiCanvas() { //built off of free-draw template fro
     });
 
     React.useEffect(() => {
-        socket.current = io.connect('wss://graffiti-websockets.herokuapp.com:3386');
+        socket.current = io.connect('wss://graffiti-websockets.herokuapp.com');
 
         socket.current.on('line', (data) => {
             setLines(currentLines => ([...currentLines, data]));
