a34adc2e04cc961cbd360ca158b02f2b415093b9
dibia004 <dibia004@umn.edu>
2023-04-10 17:35:13 -0500

    removed infinite redirects

--- client/next.config.js
+++ client/next.config.js
@@ -15,21 +15,6 @@ module.exports = withBundleAnalyzer({
         destination: "/",
         permanent: true,
       },
-      {
-        source: "/Deathball/About",
-        destination: "/deathball/about",
-        permanent: true,
-      },
-      {
-        source: "/Deathball/Leaderboard",
-        destination: "/deathball/leaderboard",
-        permanent: true,
-      },
-      {
-        source: "/Resume",
-        destination: "/resume",
-        permanent: true
-      },
     ];
   },
 });
