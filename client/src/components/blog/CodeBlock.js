76ff17b3dbe485c8bd77c3beecee8852057fac94
dibia004 <dibia004@umn.edu>
2023-05-19 03:55:18 -0500

    neumorphic code blocks

--- client/src/components/blog/CodeBlock.js
+++ client/src/components/blog/CodeBlock.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import { Box } from '@chakra-ui/react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
 import theme from 'prism-react-renderer/themes/nightOwl'
 
 const LineNumber = (props) => (
@@ -16,6 +16,7 @@ const LineNumber = (props) => (
 
 const CodeBlock = ({ children, className }) => {
   const language = className ? className.replace(/language-/, '') : 'javascript'
+  const colorMode = useColorModeValue("light", "dark");
 
   return (
     <Highlight
@@ -25,31 +26,42 @@ const CodeBlock = ({ children, className }) => {
       theme={theme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Box
-          as={'pre'}
-          className={className}
-          style={{
-            ...style,
-            padding: '10px',
-            marginBottom: '32px',
-            overflow: 'scroll',
-            textAlign: 'left',
-          }}
-          fontSize={'sm'}
-        >
-          {tokens.map((line, i) => (
-            <Box
-              verticalAlign={'center'}
-              display={'table-row'}
-              key={i}
-              {...getLineProps({ line, key: i })}
-            >
-              <LineNumber number={i + 1} />
-              {line.map((token, key) => (
-                <Box as={'span'} key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </Box>
-          ))}
+        <Box p={"10px 10px 10px 10px"} borderRadius={"2px"} boxShadow={
+          colorMode === "light" ? '20px 20px 30px #cccccc, -20px -20px 30px #ffffff'
+          : '20px 20px 30px #1b1b1b, -20px -20px 30px #252525'
+        }>
+        <Box p={"4"} borderRadius={"20px"} boxShadow={
+          colorMode === "light" ? 'inset 20px 20px 30px #cccccc, inset -20px -20px 30px #ffffff'
+          : 'inset 20px 20px 30px #1b1b1b, inset -20px -20px 30px #252525'
+        }>
+          <Box
+            as={'pre'}
+            className={className}
+            style={{
+              ...style,
+              borderRadius: "10px",
+              padding: '10px',
+              overflow: 'scroll',
+              textAlign: 'left',
+              background: "var(--dark)",
+            }}
+            fontSize={'sm'}
+          >
+            {tokens.map((line, i) => (
+              <Box
+                verticalAlign={'center'}
+                display={'table-row'}
+                key={i}
+                {...getLineProps({ line, key: i })}
+              >
+                <LineNumber number={i + 1} />
+                {line.map((token, key) => (
+                  <Box as={'span'} key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </Box>
+            ))}
+          </Box>
+        </Box>
         </Box>
       )}
     </Highlight>
