56dc8122cadf7c05d02ae651365d72f3213561f5
anthonydibi <ajdibiaggio@gmail.com>
2026-03-07 14:54:47 -0600

    no compression

--- server/api.js
+++ server/api.js
@@ -12,7 +12,6 @@ const io = socketio(server, {
     }
 })
 var cors = require('cors')
-const compression = require('compression');
 
 const { Client, types } = require('pg');
 const { json } = require('express')
@@ -33,8 +32,6 @@ app.use(
 )
 
 app.use(cors())
-
-app.use(compression())
 
 types.setTypeParser(1114, function(stringValue) {
     return stringValue;
