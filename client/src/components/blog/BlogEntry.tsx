a86b3ab34db0cd9992add2d1590b0e849211316c
anthonydibi <ajdibiaggio@gmail.com>
2026-06-09 23:09:24 -0500

    Stretch blog entry card content

--- client/src/components/blog/BlogEntry.tsx
+++ client/src/components/blog/BlogEntry.tsx
@@ -28,8 +28,10 @@ const BlogEntry: React.FC<BlogEntryProps> = ({ post, tags = true }) => {
     <>
       <LinkBox
         as={'article'}
+        display="flex"
         flexDirection={'column'}
         w={'100%'}
+        h="100%"
         textAlign={'start'}
         position={'relative'}
         borderRadius={'16px'}
@@ -39,7 +41,7 @@ const BlogEntry: React.FC<BlogEntryProps> = ({ post, tags = true }) => {
           '5px 5px 8px #1b1b1b, -5px -5px 8px #252525',
         )}
       >
-        <Flex direction="column" gap={3.5}>
+        <Flex direction="column" gap={3.5} h="100%">
           <AspectRatio ratio={16 / 9}>
             <Image
               style={{ borderRadius: '16px 16px 0 0' }}
@@ -49,26 +51,28 @@ const BlogEntry: React.FC<BlogEntryProps> = ({ post, tags = true }) => {
               fill
             />
           </AspectRatio>
-          <Flex direction="column" gap={3.5} p="0 16px 16px 16px">
-          <LinkOverlay as={NextLink} href={`/blog/posts/${post.slug}`}>
-            <Heading size="md" display={'block'}>
-              {post.title}
-            </Heading>
-          </LinkOverlay>
-          {tags && (
-            <HStack wrap={'wrap'} gap={'8px'}>
-              {post.tags &&
-                post.tags.map((tag) => (
-                  <BlogTag key={tag} tagConfigKey={tag} />
-                ))}
-            </HStack>
-          )}
-          <Text size={'sm'}>
-            <DateFormatter dateString={post.date} />
-          </Text>
-          <Text fontSize={'md'} display={['none', null, 'inherit']}>
-            {post.excerpt}
-          </Text>
+          <Flex direction="column" gap={3.5} p="0 16px 16px 16px" flex={1}>
+            <LinkOverlay as={NextLink} href={`/blog/posts/${post.slug}`}>
+              <Heading size="md" display={'block'}>
+                {post.title}
+              </Heading>
+            </LinkOverlay>
+            {tags && (
+              <HStack wrap={'wrap'} gap={'8px'}>
+                {post.tags &&
+                  post.tags.map((tag) => (
+                    <BlogTag key={tag} tagConfigKey={tag} />
+                  ))}
+              </HStack>
+            )}
+            <Text size={'sm'}>
+              <DateFormatter dateString={post.date} />
+            </Text>
+            <Flex direction="column" mt="auto">
+              <Text fontSize={'md'} display={['none', null, 'inherit']}>
+                {post.excerpt}
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
       </LinkBox>
